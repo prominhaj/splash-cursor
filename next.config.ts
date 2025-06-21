518997ac6d57f3d69c58361961ffe1644bcc4575
prominhaj <parsonal322532@gmail.com>
2025-06-22 01:26:40 +0600

    feat: update ESLint and TypeScript configurations in next.config.ts and eslint.config.mjs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // Skip ESLint during production builds
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Skip type errors during production builds
+    ignoreBuildErrors: true, // ✅ Only works in Next.js 12–13
+  },
 };
 
 export default nextConfig;
